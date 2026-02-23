9af9277de42dffee2fca59627445e20e83385715
Sherouk87 <sheroukzaki87@gmail.com>
2026-02-23 21:35:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
 import streamlit as st
-st.markdown(
-    """
-<style>
-.stApp{
-background-image:url("cropped-NTG-Clarity-Logo-pk6lqv2fycz2q45ipyjvxu52cjouaxf1yuxe4lxjk2");
-background-repeat:no-repeat;
-background-attachment:fixed;
-background-position:center;
-background-size:300px;
-opacity:0.1;
-}
-</style>
-""",
-    unsafe_allow_html=True
-)
 st.title("Candidate Evaluation System")
 tech_score=st.number_input("Enter the candidate technical score:",min_value=0.0,max_value=100.0,value=50.0,step=0.5)
 st.subheader(f"Score:{tech_score}%")
